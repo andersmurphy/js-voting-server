7a9f971369d827f37719c4716711400a21ac82a9
Anders <andersmurphy@gmail.com>
2016-12-02 22:23:29 +0000

    adds test to show immutable lists in action
    Note: Also replaces all instances of let with const
    to avoid mutation (as let is mutable).

--- __tests__/immutable.spec.js
+++ __tests__/immutable.spec.js
@@ -1,3 +1,5 @@
+import {List} from 'immutable'
+
 describe('immutability', () => {
 
   describe('a number', () => {
@@ -6,11 +8,32 @@ describe('immutability', () => {
     }
 
     it('is immutable', () => {
-      let state = 42
-      let nextState = increment(state)
+      const state = 42
+      const nextState = increment(state)
 
       expect(nextState).toEqual(43)
       expect(state).toEqual(42)
+    })
+  })
+
+  describe('a list', () => {
+    function addMovie(currentState, movie) {
+      return currentState.push(movie)
+    }
+
+    it('is immutable', () => {
+      const state = List.of('Trainspotting', '28 Days Later')
+      const nextState = addMovie(state, "Sunshine")
+
+      expect(nextState).toEqual(List.of(
+        'Trainspotting',
+        '28 Days Later',
+        'Sunshine'
+      ))
+      expect(state).toEqual(List.of(
+        'Trainspotting',
+        '28 Days Later'
+      ))
     })
   })
 })
